d2b639f8d04fa2dbda597370e7af027464774720
Patriks Raiss-Reiss <64326465+SeetZy@users.noreply.github.com>
2023-04-15 23:16:00 +0300

    qr code scanner

--- app/lib/pages/scan.code.dart
+++ app/lib/pages/scan.code.dart
@@ -1,15 +1,143 @@
 /*
   * Util/Dependency imports
  */
+import 'dart:developer';
 import 'package:flutter/material.dart';
+// ? https://pub.dev/packages/qr_code_scanner
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class ScanQrCode extends StatelessWidget {
-  const ScanQrCode({super.key});
+class ScanQrCode extends StatefulWidget {
+  const ScanQrCode({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ScanQrCodeState();
+}
+
+class _ScanQrCodeState extends State<ScanQrCode> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    controller!.resumeCamera();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Scan Qr Code'),
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30, bottom: 5),
+            child: _buildQrView(context),
+          ),
+        ),
+        Expanded(
+          flex: 0,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                const Text('Skenē QR kodu'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    // Button to turn on the camera flash
+                    Container(
+                      margin: const EdgeInsets.all(8),
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.toggleFlash();
+                            setState(() {});
+                          },
+                          child: FutureBuilder(
+                            future: controller?.getFlashStatus(),
+                            builder: (context, snapshot) {
+                              return const Icon(Icons.flash_auto_outlined);
+                            },
+                          )),
+                    ),
+
+                    // Button to change the camera view
+                    Container(
+                      margin: const EdgeInsets.all(8),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await controller?.flipCamera();
+                          setState(() {});
+                        },
+                        child: FutureBuilder(
+                          future: controller?.getCameraInfo(),
+                          builder: (context, snapshot) {
+                            if (snapshot.data != null) {
+                              return const Icon(Icons.cameraswitch_outlined);
+                            } else {
+                              return const Text('loading');
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
     );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.green,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
