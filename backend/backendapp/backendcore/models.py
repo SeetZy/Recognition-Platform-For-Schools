f7a88e8cb156e0b9961356d703c0c3baf416c477
FTyumin <feodor.tjumin28@gmail.com>
2023-05-01 16:46:49 +0300

    Edits

--- backend/backendapp/backendcore/models.py
+++ backend/backendapp/backendcore/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class Local(models.Model):
     _DATABASE = 'default'
+    name = models.CharField(max_length=50)
+    data = models.JSONField()
     
 class Remote(models.Model):
     _DATABASE = 'remote'
+    name = models.CharField(max_length=20)
+    data = models.JSONField()
      
