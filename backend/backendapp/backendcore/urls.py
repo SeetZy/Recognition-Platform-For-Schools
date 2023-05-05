e9535d0feb8e1a00cbd26ed218383a6dee7b81d2
FTyumin <feodor.tjumin28@gmail.com>
2023-05-05 22:04:12 +0300

    Edits

--- backend/backendapp/backendcore/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import UserRecordView
-
-app_name = 'backendcore'
-urlpatterns =[
-    path('hellodjango', views.hello_django, name='hellodjango'),
-    path('user/', UserRecordView.as_view(), name='users')
-]
