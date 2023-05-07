2b6f1f581276c6f6d32e3a0a81dd4cf39a556a5e
FTyumin <feodor.tjumin28@gmail.com>
2023-05-07 17:44:11 +0300

    Added user groups

--- backend/backendapp/app/models.py
+++ backend/backendapp/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    # Add your custom fields here if needed
+    pass
---
+++ backend/backendapp/app/user_groups.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
+
+User = get_user_model()
+user = User.objects.get(username='example_student')
+group = Group.objects.get(name='Students')
+user.groups.add(group)
+
+user = User.objects.get(username='example_teacher')
+group = Group.objects.get(name='Teachers')
+user.groups.add(group)
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='Students').exists(), login_url='/')
+def student_dashboard(request):
+    text= "Welcome students"
+    return render(request, 'student_dashboard.html',{'text':text})
+
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='Teachers').exists(), login_url='/')
+def teacher_dashboard(request):
+    # This view is accessible only to users in the "Teachers" group
+    # Add your logic here
+    pass
